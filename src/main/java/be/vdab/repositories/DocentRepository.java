6754554fc15275a5a4303655358e574a535fddca
kjell.leenknegt <kjell.leenknegt@OntCur-0E05C437.vdabheverlee.intra>
2017-02-14 13:30:34 +0100

    toevoegen opslag, vanTotWedde, voornamen, docentVerwijderen

--- src/main/java/be/vdab/repositories/DocentRepository.java
+++ src/main/java/be/vdab/repositories/DocentRepository.java
@@ -1,18 +1,42 @@
 package be.vdab.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
 import be.vdab.entities.Docent;
 
-public class DocentRepository {
-	
-	public Optional<Docent> read(long id, EntityManager entityManager) {
-		return Optional.ofNullable(entityManager.find(Docent.class, id));
-		}
-	
-	public void create(Docent docent, EntityManager entityManager) {
-		entityManager.persist(docent);
-		}
+public class DocentRepository extends AbstractRepository {
+public Optional<Docent> read(long id) {
+return Optional.ofNullable(getEntityManager().find(Docent.class, id));
+}
+
+public void create(Docent docent) {
+getEntityManager().persist(docent);
+}
+
+public void delete(long id) {
+read(id).ifPresent(docent -> getEntityManager().remove(docent));
+}
+
+public List<Docent> findByWeddeBetween(BigDecimal van, BigDecimal tot,
+int vanafRij, int aantalRijen) {
+return getEntityManager().createNamedQuery("Docent.findByWeddeBetween", Docent.class)
+.setParameter("van", van)
+.setParameter("tot", tot)
+.setFirstResult(vanafRij)
+.setMaxResults(aantalRijen)
+.getResultList();
+}
+
+public List<String> findVoornamen() {
+return getEntityManager().createQuery(
+"select d.voornaam from Docent d", String.class).getResultList();
+}
+
+public BigDecimal findMaxWedde() {
+return getEntityManager().createQuery(
+"select max(d.wedde) from Docent d", BigDecimal.class).getSingleResult();
+}
 
 }
--- src/main/java/be/vdab/services/DocentService.java
+++ src/main/java/be/vdab/services/DocentService.java
@@ -1,41 +1,62 @@
 package be.vdab.services;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 
 import be.vdab.entities.Docent;
-import be.vdab.filters.JPAFilter;
 import be.vdab.repositories.DocentRepository;
 
-public class DocentService {
-
+public class DocentService extends AbstractService {
 	private final DocentRepository docentRepository = new DocentRepository();
 	public Optional<Docent> read(long id) {
-	EntityManager entityManager = JPAFilter.getEntityManager();
-	try {
-	return docentRepository.read(id, entityManager);
-	} 
-	finally {
-	entityManager.close();
-	}
-	}
+		return docentRepository.read(id);
+		}
 	
 	public void create(Docent docent) {
-	EntityManager entityManager = JPAFilter.getEntityManager();
-	entityManager.getTransaction().begin();
-	try {
-	docentRepository.create(docent, entityManager);
-	entityManager.getTransaction().commit();
-	} catch (PersistenceException ex) {
-	entityManager.getTransaction().rollback();
-	throw ex;
-	} 
-	finally {
-	entityManager.close();
-	}
-
-	}
-
+		beginTransaction();
+		try {
+		docentRepository.create(docent);
+		commit();
+		} catch (PersistenceException ex) {
+		rollback();
+		throw ex;
+		}
+		}
+	
+	public void delete(long id) {
+		beginTransaction();
+		try {
+		docentRepository.delete(id);
+		commit();
+		}
+		catch (PersistenceException ex) {
+		rollback();
+		throw ex;
+		}
+		}
+	
+	public void opslag(long id, BigDecimal percentage) {
+		beginTransaction();
+		try {
+		docentRepository.read(id).ifPresent(docent -> docent.opslag(percentage));
+		commit();
+		} catch (PersistenceException ex) {
+		rollback();
+		throw ex;
+		}
+		}
+	public List<Docent> findByWeddeBetween(
+		BigDecimal van, BigDecimal tot, int vanafRij, int aantalRijen) {
+		return docentRepository.findByWeddeBetween(van, tot, vanafRij, aantalRijen);
+		}
+	public List<String> findVoornamen() {
+		return docentRepository.findVoornamen();
+		}
+	
+	public BigDecimal findMaxWedde() {
+		return docentRepository.findMaxWedde();
+		}
 }
